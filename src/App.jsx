b8cbd840072f1465c4f9edf7e61932b31c7be106
AndreasClerrmont <ac@clermontdesignklassiker.de>
2026-05-10 18:31:54 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,21 @@
-export default function App() {
+ </div>
+
+          <div className="bg-gray-50 rounded-2xl p-4 shadow">
+            <h2 className="font-bold text-xl">Dienstag</h2>
+            <p>Explosivität & Athletik</p>
+          </div>
+
+          <div className="bg-gray-50 rounded-2xl p-4 shadow">
+            <h2 className="font-bold text-xl">Donnerstag</h2>
+            <p>Volumen & Stabilität</p>
+          </div>
+
+          <div className="bg-gray-50 rounded-2xl p-4 shadow">
+            <h2 className="font-bold text-xl">Freitag</h2>
+            <p>Power Endurance</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
