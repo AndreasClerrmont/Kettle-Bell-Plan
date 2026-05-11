99eb980279191f88c63820d55c8a1c504b83e7cc
AndreasClerrmont <ac@clermontdesignklassiker.de>
2026-05-11 18:00:12 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
+import FemalePlan from "./FemalePlan.jsx";
 import ReactDOM from "react-dom/client";
-import KettlebellTrainingApp from "./FemalePlan.jsx";
+import KettlebellTrainingApp from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
